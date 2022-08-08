e9e1baa0108f5a00c43b6aa16df431df2439f6c8
unknown <gabbjuncos@gmail.com>
2022-08-08 14:03:26 -0300

    LISTO HASTA 14.5

--- HeroesApp/src/app/material/material.module.ts
+++ HeroesApp/src/app/material/material.module.ts
@@ -8,9 +8,9 @@ import {MatCardModule} from '@angular/material/card';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input'
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   exports: [
@@ -24,7 +24,8 @@ import {MatInputModule} from '@angular/material/input'
     MatGridListModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
 
   ]
 })
