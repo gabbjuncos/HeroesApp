e02bff699d31d60f70cb7548e43a6c30600aabc4
unknown <gabbjuncos@gmail.com>
2022-08-11 13:40:20 -0300

    LISTO HASTA 14.9

--- HeroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ HeroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {switchMap} from 'rxjs/operators'
+
 import { Heroe, Publisher } from '../../intefaces/heroes.interface';
 import { HeroesService } from '../heroe/services/heroes.service';
+
+
 
 @Component({
   selector: 'app-agregar',
@@ -30,9 +35,25 @@ export class AgregarComponent implements OnInit {
     
   }
 
-  constructor( private heroeService: HeroesService) { }
+  constructor( private heroeService: HeroesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+
+    //me fijo si en la url que obtiene esta la palabra editar
+    // si no se encuenta la palabra editar devolveria undefinied asi que que salga y no devuelva nada
+    if(!this.router.url.includes('editar')){
+      return
+    }
+
+    // obtengo id de la url
+    // cuando se ingrese por agregar el id es undefinid y cuando es por editar toma el id del personaje
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroeService.getHeroePorId(id) )
+        )
+      .subscribe( heroe => this.heroe = heroe );
   }
 
   guardar(){
@@ -40,11 +61,25 @@ export class AgregarComponent implements OnInit {
       return;
     }
 
-    this.heroeService.agregarHeroe(this.heroe)
-      .subscribe( resp => {
-        console.log('Respuesta', resp);
+    if (this.heroe.id){
+      //actualizar
+      this.heroeService.actualizarHeroe(this.heroe)
+        .subscribe(heroe => console.log('Actualizando', heroe ))
+      console.log(this.heroe.id)
+    }else{
+      // crear
+      this.heroeService.agregarHeroe(this.heroe)
+        .subscribe( heroe => {
+          this.router.navigate(['/heroes/editar', heroe.id]);
+          console.log('Creando', heroe);
       })
-    
+    }
   }
 
+  borrarHeroe(){
+    this.heroeService.borrarHeroe(this.heroe.id!)
+      .subscribe( resp => {
+        this.router.navigate(['/heroes'])
+      });
+  }
 }
--- HeroesApp/src/app/heroes/pipes/imagen.pipe.ts
+++ HeroesApp/src/app/heroes/pipes/imagen.pipe.ts
@@ -2,11 +2,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../intefaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  // comportamiento por defecto
+  //pure: true 
 })
 export class ImagenPipe implements PipeTransform {
 
   transform(heroe: Heroe): string {
+    console.log('pipe imagen se proceso');
+    // si no tiene id es porque se esta creando y se devuelve la imagen con la que se indica que no hay imagen
+    if(!heroe.id){
+      return 'assets/no-image.jpg';
+    }
+    // si tiene url cargada devolverla
+    else if(heroe.alt_img){
+      return heroe.alt_img;
+    }
+
     console.log(heroe)
     return `assets/heroes/${heroe.id}.jpg`;
   }
